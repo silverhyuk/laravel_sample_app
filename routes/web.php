9a065a4b847d2a49e7a9d362a2a2718c2416f378
silverhyuk84 <silverhyuk84@gmail.com>
2018-12-28 07:03:03 +0900

    route 살펴보기

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,54 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*Route::get('welcome', function () {
+    return 'welcome';
+});*/
+
+Route::get('welcome/{postId?}', function ($postId = 1) {
+    return 'post Id : '. $postId;
+})->name('welcomeName');
+
+/*Route::get('test', function () {
+   return redirect()->route('welcomeName');
+});*/
+
+Route::get('test', 'WelcomeController@test')->name('testName');
+
+Route::get('welcome/{postId}/comments/{commentId}', function ($postId, $commentId) {
+    return 'PostId : ' . $postId . ', CommentId : ' . $commentId;
+});
+
+/*Route::get('dashboard', function () {
+    return 'Dash Board';
+})->middleware('auth');
+
+Route::get('user/profile', function () {
+    return 'Dash Board';
+})->middleware('auth');*/
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('dashboard', function () {
+        return 'Dash Board';
+    });
+
+    Route::get('user/profile', function () {
+        return 'Dash Board';
+    });
+});
+
+
+// Namespace
+Route::group(['namespace' => 'Admin'], function () {
+    //  namespace App\Http\Controllers\Admin;
+});
+
+//Prefix
+Route::group(['prefix' => 'system'], function () {
+    Route::get('show', function () {
+       // URL : system/show
+    });
+});
+
+//php artisan route:list
